3da80e4428c3fb365d93b71b6b83b26a0a78164d
IsackAvila <isackbaptistella24@gmail.com>
2026-04-06 13:40:27 -0300

    Update millis_usado_com_DOIS_botao.ino

--- aula 6/millis_usado_com_DOIS_botao/millis_usado_com_DOIS_botao.ino
+++ aula 6/millis_usado_com_DOIS_botao/millis_usado_com_DOIS_botao.ino
@@ -12,45 +12,41 @@ unsigned long tempo_debounce = 0;
 unsigned long atraso = 50;
 
 void setup() {
-pinMode(pin_botaoA, INPUT_PULLUP);
-pinMode(pin_ledA, OUTPUT);
-digitalWrite(pin_ledA, estado_ledA);
-pinMode(pin_botaoB, INPUT_PULLUP);
-pinMode(pin_ledB, OUTPUT);
-digitalWrite(pin_ledB, estado_ledB);
-
-
+  pinMode(pin_botaoA, INPUT_PULLUP);
+  pinMode(pin_ledA, OUTPUT);
+  digitalWrite(pin_ledA, estado_ledA);
+  pinMode(pin_botaoB, INPUT_PULLUP);
+  pinMode(pin_ledB, OUTPUT);
+  digitalWrite(pin_ledB, estado_ledB);
 }
 
 void loop() {
-int leituraA = digitalRead(pin_botaoA);
-int leituraB = digitalRead(pin_botaoB);
-if (leituraA != ultimo_estado_botaoA) {
-  tempo_debounce = millis();
+  int leituraA = digitalRead(pin_botaoA);
   int leituraB = digitalRead(pin_botaoB);
-if (leituraB != ultimo_estado_botaoB) {
-  tempo_debounce = millis();
-  
-}
-}
-if ((millis() - tempo_debounce) > atraso)
-{
-  if (leituraA != estado_botaoA) {
-    estado_botaoA = leituraA;
-    if (estado_botaoA == LOW) {
-      estado_ledA = !estado_ledA;
-
-      if (leituraB != estado_botaoB) {
-    estado_botaoB = leituraB;
-    if (estado_botaoB == LOW) {
-      estado_ledB = !estado_ledB;
+  if (leituraA != ultimo_estado_botaoA) {
+    tempo_debounce = millis();
+    int leituraB = digitalRead(pin_botaoB);
+    if (leituraB != ultimo_estado_botaoB) {
+      tempo_debounce = millis();
     }
   }
-}
-digitalWrite(pin_ledA, estado_ledA);
-ultimo_estado_botaoA = leituraA;
-digitalWrite(pin_ledB, estado_ledB);
-ultimo_estado_botaoB = leituraB;
-}
-}
+  if ((millis() - tempo_debounce) > atraso) {
+    if (leituraA != estado_botaoA) {
+      estado_botaoA = leituraA;
+      if (estado_botaoA == LOW) {
+        estado_ledA = !estado_ledA;
+
+        if (leituraB != estado_botaoB) {
+          estado_botaoB = leituraB;
+          if (estado_botaoB == LOW) {
+            estado_ledB = !estado_ledB;
+          }
+        }
+      }
+      digitalWrite(pin_ledA, estado_ledA);
+      ultimo_estado_botaoA = leituraA;
+      digitalWrite(pin_ledB, estado_ledB);
+      ultimo_estado_botaoB = leituraB;
+    }
+  }
 }
